a29840f9be4c0bfe70d3ee3269d0952166be1b96
mackbr96 <mackbr@clarkson.edu>
2018-09-12 14:19:35 -0400

    Added not switches

--- mainTest.py
+++ mainTest.py
@@ -8,24 +8,47 @@ class Switch:
     def add(self, num):
         self.depend.append(num)
 
+class NotSwitch:
+    def __init__(self):
+        self.depend = []
+    
+    def add(self, num):
+        self.depend.append(num)
+
+
 
 def graph(bulbs, switches):
     sw = []
-    for i in range(0,len(bulbs)):
+    nsw = []
+    for i in range(0,len(switches)):
         s = Switch()
+        ns = NotSwitch()
         sw.append(s)
+        nsw.append(ns)
+
 
     for i in range(0, len(bulbs)):
         first = bulbs[i][0]
         second = bulbs[i][1]
-        sw[abs(first) - 1].add(second * -1)
-        sw[abs(second) - 1].add(first * -1)
+
+        if( first > 0):
+            sw[abs(first) - 1].add(second * -1)
+        else:
+            nsw[abs(first) - 1].add(second * -1)
+
+        if( second > 0):
+            sw[abs(second) - 1].add(first * -1)
+        else:
+            nsw[abs(second) - 1].add(first * -1)
         '''
             Not second depends on first
             Not first depends on second
         '''
+
     for i in range(0,len(sw)):
-        print(sw[i].depend)
+        print("Switch " + str(i + 1) + " depends on " + str(sw[i].depend))
+        print("Switch NOT " + str(i + 1) + " depends on " + str(nsw[i].depend))
+
 
 def checkAll(swithces, bulbs):
     global bulbNum
